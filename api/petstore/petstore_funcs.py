86c13633a84abcc4a7e9ccd8a7452d1a36e56f2f
Giant Whale <github@beautiful-electricity.com>
2021-06-18 18:48:14 +0300

    main - random userStatus

--- api/petstore/petstore_funcs.py
+++ api/petstore/petstore_funcs.py
@@ -1,6 +1,10 @@
 from api.petstore.petstore_api import request_user_create
 from api.petstore.petstore_models import User
-from core.utils import random_string, random_email
+from core.utils import (
+    random_string,
+    random_email,
+    random_int32,
+)
 
 
 def create_random_user() -> User:
@@ -11,7 +15,7 @@ def create_random_user() -> User:
         email=random_email(),
         password=random_string(),
         phone=random_string(),
-        userStatus=1
+        userStatus=random_int32()
     )
     request_user_create(user=user)
     return user
