deb222e260b710c4151b8641f76ca6b71383d103
Giant Whale <github@beautiful-electricity.com>
2021-06-18 19:27:03 +0300

    main - request user_update and default waiting time

--- api/petstore/petstore_api.py
+++ api/petstore/petstore_api.py
@@ -1,6 +1,7 @@
 import allure
 
 from api.petstore.petstore_models import User
+from core import settings
 from core.request import make_request
 import time
 
@@ -15,7 +16,7 @@ def request_user_create(user: User):
         response_json = response.json()
         user.id = int(response_json['message'])
     with allure.step('Waiting for changes after user was created'):
-        time.sleep(1)
+        time.sleep(settings.WAIT_TIME)
     return response
 
 
@@ -24,14 +25,16 @@ def request_user_get(username: str):
     return response
 
 
-def request_user_update(user: User):
+def request_user_update(username: str, updated_user: User):
+    body = updated_user.get_user()
+    response = make_request(method='PUT', url=BASE_URL + f'v2/user/{username}', json=body)
     with allure.step('Waiting for changes after user was updated'):
-        time.sleep(1)
-    pass
+        time.sleep(settings.WAIT_TIME)
+    return response
 
 
 def request_user_delete(username: str):
     response = make_request(method='DELETE', url=BASE_URL + f'v2/user/{username}')
     with allure.step('Waiting for changes after user was deleted'):
-        time.sleep(1)
+        time.sleep(settings.WAIT_TIME)
     return response
